da08c5a06552b3adc213219a2a0708530c233485
Amrutha P <amrutharamakumar@gmail.com>
2024-02-12 11:43:30 +0530

    Commit

--- js/scripts.js
+++ js/scripts.js
@@ -48,3 +48,58 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+//Form Validation 
+var nameError = document.getElementById("nameError")
+var emailError = document.getElementById("emailError")
+var phoneError = document.getElementById("phoneError")
+var messageError = document.getElementById("messageError")
+function validateName(){
+    var name=document.getElementById("name").value 
+    if(name == ""){
+       nameError.innerHTML = "Name is required"
+    }
+    else if(name.length<4){
+        nameError.innerHTML = "minlength is 4"
+    }
+    else{
+        nameError.innerHTML = ""
+    }
+}
+function validateEmail(){
+    var email=document.getElementById("email").value 
+    if(email == ""){
+        emailError.innerHTML = "Email is required"
+    }
+    else if(!email.includes("@")){
+        emailError.innerHTML = "@ required"
+    }
+    else{
+        emailError.innerHTML = ""
+    }
+}
+function validatePhone(){
+    var phone=document.getElementById("phone").value 
+    if(phone == ""){
+        phoneError.innerHTML = "Phone is required"
+    }
+    else if(phone.length<10 || phone.length>10){
+        phoneError.innerHTML = "10 elements required"
+    }
+    else{
+        phoneError.innerHTML = ""
+    }
+}
+function validateMessage(){
+    var message=document.getElementById("message").value 
+    if(phone == ""){
+        messageError.innerHTML = "MEssage is required"
+    }
+    else if(message.length<20){
+        messageError.innerHTML = "20 characters required"
+    }
+    else{
+        messageError.innerHTML = ""
+    }
+}
